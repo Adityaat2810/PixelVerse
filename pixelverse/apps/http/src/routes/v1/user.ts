7393607da7c897ae65400a327a1bacc1309cc12e
Aditya Kumar Saini <adityasainics10@gmail.com>
2025-02-02 01:21:17 +0530

    create user metadata route

--- pixelverse/apps/http/src/routes/v1/user.ts
+++ pixelverse/apps/http/src/routes/v1/user.ts
@@ -1,10 +1,53 @@
 import { Router } from "express";
+import { UpdateMetadataSchema } from "../../types";
+import client from "@repo/db/client"
+import { userMiddleware } from "../../middleware/user";
 export const userRouter = Router()
 
-userRouter.post('/metadata',(req,res)=>{
+userRouter.post('/metadata',userMiddleware,async (req,res)=>{
+  const parseData = UpdateMetadataSchema.safeParse(req.body)
+  if(!parseData.success){
+    res.status(400).json({
+      message:"Validation Failed"
+    })
+    return;
+  }
+  try {
+    await client.user.update({
+      where: {
+        id: req.userId
+      },
+      data: {
+        avatarId: parseData.data.avatarId
+      }
+    })
+    res.json({message: "Metadata updated"})
+  } catch(e) {
+    console.log("error")
+    res.status(400).json({message: "Internal server error"})
+  }
 
 })
 
-userRouter.get('/metadata/bulk',(req,res)=>{
-  
+userRouter.get("/metadata/bulk", async (req, res) => {
+  const userIdString = (req.query.ids ?? "[]") as string;
+  const userIds = (userIdString).slice(1, userIdString?.length - 1).split(",");
+  console.log(userIds)
+  const metadata = await client.user.findMany({
+    where: {
+      id: {
+        in: userIds
+      }
+    }, select: {
+      avatar: true,
+      id: true
+    }
+  })
+
+  res.json({
+    avatars: metadata.map(m => ({
+      userId: m.id,
+      avatarId: m.avatar?.imageUrl
+    }))
+  })
 })
