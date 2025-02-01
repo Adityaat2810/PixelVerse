5121c10b1c6c193479434d6222a0585600b7cbae
Aditya Kumar Saini <adityasainics10@gmail.com>
2025-02-01 14:35:41 +0530

    setup prisma and design schema

--- pixelverse/apps/http/src/index.ts
+++ pixelverse/apps/http/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import { router } from "./routes/v1";
 const app = express();
+import client from "@repo/db/client"
 
 app.use('/api/v1',router);
 
--- pixelverse/apps/http/src/routes/v1/index.ts
+++ pixelverse/apps/http/src/routes/v1/index.ts
@@ -1,15 +1,30 @@
 import { Router } from "express";
+import { userRouter } from "./user";
+import { adminRouter } from "./admin";
+import { spaceRouter } from "./space";
 export const router = Router();
 
-router.get('/signup',(req,res)=>{
+router.post('/signup',(req,res)=>{
   res.json({
     message:"signup"
   })
 
 })
 
-router.get('/signin',(req,res)=>{
+router.post('/signin',(req,res)=>{
   res.json({
     message:"signin"
   })
 })
+
+router.get('/elements',(req,res)=>{
+
+})
+
+router.get('/avatar',(req,res)=>{
+
+})
+
+router.use('/user',userRouter)
+router.use('/space',spaceRouter)
+router.use('/admin',adminRouter)
